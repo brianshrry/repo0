935bbabc02f80fc4683c6bf4a3b3c7d0ee3c7aeb
brianshrry <150218172+brianshrry@users.noreply.github.com>
2023-11-15 17:20:26 -0500

    Add files via upload
    added test case scripts

---
+++ test_cases.cy.js
@@ -0,0 +1,58 @@
+describe('sauce demo', () => {
+  it('passes', () => {
+      cy.visit('https://www.saucedemo.com');
+      cy.get('[data-test="username"]').type('standard_user');
+      cy.get('[data-test="password"]').type('secret_sauce');
+      cy.get('[data-test="login-button"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+      cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
+      cy.get('.shopping_cart_link').click();
+      cy.get('[data-test="checkout"]').click();
+      cy.get('[data-test="firstName"]').type('first');
+      cy.get('[data-test="lastName"]').type('last');
+      cy.get('[data-test="postalCode"]').type('12345');
+      cy.get('[data-test="continue"]').click();
+      cy.get('[data-test="finish"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+      cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
+      cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+      cy.get('[data-test="remove-sauce-labs-bike-light"]').click();
+      cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+      cy.get('[data-test="remove-sauce-labs-fleece-jacket"]').click();
+      cy.get('[data-test="remove-sauce-labs-onesie"]').click();
+      cy.get('[data-test="remove-test.allthethings()-t-shirt-(red)"]').click();
+      cy.get('#item_4_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+      cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('#item_0_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+      cy.get('[data-test="remove-sauce-labs-bike-light"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('#item_1_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+      cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('#item_5_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+      cy.get('[data-test="remove-sauce-labs-fleece-jacket"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('#item_2_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+      cy.get('[data-test="remove-sauce-labs-onesie"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+      cy.get('#item_3_title_link > .inventory_item_name').click();
+      cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
+      cy.get('[data-test="remove-test.allthethings()-t-shirt-(red)"]').click();
+      cy.get('[data-test="back-to-products"]').click();
+  })
+})
